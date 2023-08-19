95176def8ed9b2387a871a5e0578ad4604bab937
Don-Luka <140162151+Don-Luka@users.noreply.github.com>
2023-08-19 15:56:14 +0200

    Version 0.2 - complete functions - basic level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+import sys
+import random
 
 def delta_time(f):
     def wrapper(*args, **kwargs):
@@ -19,8 +21,11 @@ class MotionPicture():
 
         self.views_number = 0
 
-    def play(self):
-        self.views_number += 1
+    def __repr__(self) -> str:
+        return f'{self.title} ({self.year})'
+
+    def play(self, views = 1):
+        self.views_number += views
 
 class Movie(MotionPicture):
     pass
@@ -33,40 +38,64 @@ class Series(MotionPicture):
     def __repr__(self) -> str:
         return f'{self.title} S{self.season_nr:02d}E{self.episode_nr:02d}'
 
-def get_movies():
-    pass
+def get_movies(given_list):
+    return sorted([watch for watch in given_list if isinstance(watch, Movie)], key = lambda item: item.title)
 
-def get_series():
-    pass
+def get_series(given_list):
+    return sorted([watch for watch in given_list if isinstance(watch, Series)], key = lambda item: item.title)
 
 def search():
     pass
 
-def generate_views():
-    pass
+def repeat_n_times(repeats = 10):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for repeat in range(repeats):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
 
-def top_titles(number_of, content_type):
-    pass
+@repeat_n_times(repeats=10)
+def generate_views(watch_list_):
+    watch_list_[random.randint(0,len(watch_list_)-1)].play(random.randint(0,100))
 
-simpsons  = Series(season_nr=1,episode_nr=1, title='The Simpsons', year = 1994, genre = 'comedy')
+def top_titles(watch_list, number_of_items, content_type):
+    if content_type == 'movies':
+        watch_list = get_movies(watch_list)
+    elif content_type == 'series':
+        watch_list = get_series(watch_list)
+    tops_list = sorted(watch_list, key = lambda item: item.views_number)[::-1]
+    return tops_list[:min(number_of_items, len(tops_list))]
 
-print(simpsons)
 
-def main():
-    # films = {}
-    # films.update({"title": "The Shawshank Redemption", "year": 1994, "genre": "drama"})
+movies_list = []
+series_list = []
 
-    # print(films['title'])
-    # print(type(films))
+movies_list.append(('The Shawshank Redemption', 1994, 'drama'))
+movies_list.append(('The Godfather', 1972, 'crime,drama'))
+movies_list.append(('Toy Story', 1995, 'animation'))
+movies_list.append(('The Blair Witch Project', 1999, 'horror'))
 
-    film_1 = Movie('The Shawshank Redemption', 1994, 'Drama')
+series_list.append(('The Simpsons', 1989, 'animation,comedy', 20, 12))
+series_list.append(('Twin Peaks', 1990, 'crime, drama', 3, 10))
+series_list.append(('Friends', 1994, 'comedy, romance', 10, 15))
+series_list.append(('Breaking Bad', 2008, 'crime,drama', 5, 16))
 
-    print(film_1.views_number)
-    film_1.play()
-    film_1.play()
-    film_1.play()
-    film_1.play()
-    film_1.play()
-    print(film_1.views_number)
-    
-main()
+
+overall_list = movies_list + series_list
+
+movies = [Movie(title=movie[0], year = movie[1], genre = movie[2]) for movie in movies_list]
+series = [Series(season_nr = random.randint(1,serie[3]), episode_nr = random.randint(1,serie[4]), 
+        title=serie[0], year = serie[1], 
+        genre = serie[2]) for serie in series_list]
+
+watch_list = movies + series
+
+generate_views(watch_list)
+
+print(top_titles(watch_list, 2, 'series'))
+print(top_titles(watch_list, 2, 'movies'))
+print(get_movies(watch_list))
+print(get_series(watch_list))
+
